4a5d50bdd9c86d55617483f0825390c296f83faf
RJ_Raghu_tech <91701745+rjraghutech@users.noreply.github.com>
2022-10-10 20:15:05 +0530

    commit 7

--- app.js
+++ app.js
@@ -1,21 +1,42 @@
-let increase = document.querySelector(".increase");
-let decrease = document.querySelector(".decrease");
-let reset = document.querySelector(".reset");
+// let increase = document.querySelector(".increase");
+// let decrease = document.querySelector(".decrease");
+// let reset = document.querySelector(".reset");
 let num = document.querySelector(".num");
+let btns = document.querySelectorAll('.btn');
 
-increase.addEventListener("click", function(e){
-   let a = num.textContent++;
-    console.log(a); 
+// increase.addEventListener("click", function(e){
+//    let a = num.textContent++;
+//     console.log(a); 
+// });
+
+// decrease.addEventListener("click", function(e){
+//     let a = num.textContent--;
+//      console.log(a); 
+//  });
+
+//  reset.addEventListener("click", function(e){
+//     let a = num.textContent = 0;
+//      console.log(a); 
+//  });
+
+btns.forEach(function (btn) {
+    btn.addEventListener('click',function(e) {
+        let pressedBtn = e.currentTarget.classList;
+        console.log(pressedBtn)
+        if(pressedBtn.contains('increase')){
+            num.textContent++;
+        }
+        else if(pressedBtn.contains('decrease')){
+            num.textContent--;
+        }
+        else{
+            num.textContent = 0;
+        }
+        if(num.textContent == 0)
+            num.style.color = "black";
+        else if(num.textContent >0)
+            num.style.color = "green";
+        else
+            num.style.color = "red";
+    });
 });
-
-decrease.addEventListener("click", function(e){
-    let a = num.textContent--;
-     console.log(a); 
- });
-
- reset.addEventListener("click", function(e){
-    let a = num.textContent = 0;
-     console.log(a); 
- });
-
- 
