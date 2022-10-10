a07495cf3d1d318a2e29f677644ef43c3f10c5ac
RJ_Raghu_tech <91701745+rjraghutech@users.noreply.github.com>
2022-10-10 12:13:19 +0530

    commit 3

---
+++ app.js
@@ -0,0 +1,8 @@
+let increase = document.querySelector(".increase");
+let decrease = document.querySelector(".decrease");
+let reset = document.querySelector(".reset");
+let num = document.querySelector(".num");
+
+increase.addEventListener("click", function(e){
+    
+})
