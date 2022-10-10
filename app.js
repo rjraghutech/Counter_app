9b944d2d704ea2af0bf31628c79a6b551dd6175b
RJ_Raghu_tech <91701745+rjraghutech@users.noreply.github.com>
2022-10-10 19:57:51 +0530

    commit 5

--- app.js
+++ app.js
@@ -4,5 +4,18 @@ let reset = document.querySelector(".reset");
 let num = document.querySelector(".num");
 
 increase.addEventListener("click", function(e){
-    
-})
+   let a = num.textContent++;
+    console.log(a); 
+});
+
+decrease.addEventListener("click", function(e){
+    let a = num.textContent--;
+     console.log(a); 
+ });
+
+ reset.addEventListener("click", function(e){
+    let a = num.textContent = 0;
+     console.log(a); 
+ });
+
+ 
